8c7e36bb4d53aa77ed0a914e7ab308e6476eeb46
Lucas Lee <freedom@Lucass-MacBook.local>
2019-05-14 22:42:25 +0800

    Updates

---
+++ precache-manifest.2dfd65505a01f09ed305b291f5ebfa13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "13b33f5c2c4833dfcd28",
+    "url": "/resume/static/js/main.e9386dfe.chunk.js"
+  },
+  {
+    "revision": "eb7d2773bb8e3c932686",
+    "url": "/resume/static/js/2.412c97cf.chunk.js"
+  },
+  {
+    "revision": "13b33f5c2c4833dfcd28",
+    "url": "/resume/static/css/main.50236874.chunk.css"
+  },
+  {
+    "revision": "fdb625b610a2ddbfa9e111876ae3187a",
+    "url": "/resume/index.html"
+  }
+];
